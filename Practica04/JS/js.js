11b9b5c0eb1de7f98af0352e80ae75c90c989e02
Joel Blanco <joelb5700@gmail.com>
2024-09-24 12:53:01 +0200

    6 hecho

--- Practica04/JS/js.js
+++ Practica04/JS/js.js
@@ -1,22 +1,33 @@
 window.onload = inicio;
+
 function inicio() 
 {
     document.formulario.convertir.onclick = calcular;
 }
-function convertir(base)
+
+function convertir(base) 
 {
-    let num= Number( document.formulario.decimal.value);
+    let num = Number(document.formulario.decimal.value);
     let numStr = "";
-    while (num >= 1)
-        if (Math.floor((num%base))> 9 && base == 16) 
-            numStr += String.fromCharCode(65 + (Math.floor((num%base))-10));      
-        else
-            numStr = Math.floor((num%base)) + numStr;
-        num = num/base; 
-    
+
+    while (num >= 1) 
+        {
+        if (Math.floor((num % base)) > 9 && base == 16) 
+        {
+
+            numStr = String.fromCharCode(65 + (Math.floor((num % base)) - 10)) + numStr;
+        } else 
+        {
+            numStr = Math.floor(num % base) + numStr;
+        }
+        num = Math.floor(num / base); 
+    }
     return numStr;
 }
-function calcular()
-{
 
+function calcular()
+ {
+    document.formulario.binario.value = convertir(2);
+    document.formulario.octal.value = convertir(8);
+    document.formulario.hexadecimal.value = convertir(16);
 }
--- Practica05/JS/js.js
+++ Practica05/JS/js.js
@@ -1,24 +1,61 @@
 window.onload = inicio;
 function inicio() 
 {
-    document.formulario.convertir.onclick = calcular;
+    document.formulario.comprobar.onclick = comprobar;
 }
-function convertir(base)
+function arrayTexto()
 {
-    let num= Number( document.formulario.decimal.value);
-    let numStr = "";
-    while (num >= 1)
-        if (Math.floor((num%base))> 9 && base == 16) 
-            numStr += String.fromCharCode(65 + (Math.floor((num%base))-10));      
-        else
-            numStr = Math.floor((num%base)) + numStr;
-        num = num/base; 
-    
-    return numStr;
+    let textoIntroducido = document.formulario.cadena.value;
+    textoIntroducido = textoIntroducido.toLowerCase().split("");
+    return textoIntroducido;
 }
-function calcular()
+function contar(letra)
 {
-    document.formulario.binario.value = convertir(2);
-    document.formulario.octal.value = convertir(8);
-    document.formulario.hexadecimal.value = convertir(16);
+    let texto = arrayTexto();
+    let contador = 0;
+    for (let i = 0; i < texto.length; i++) {
+        if (letra == texto[i]) 
+            contador += 1;
+    }
+    return contador;
+}
+function contarVocalesTotales(letras)
+{
+    let texto = arrayTexto();
+    let contador = 0;
+    for (let i = 0; i < texto.length; i++) {
+        for (let j = 0; j < letras.length; j++) {
+            if (letras[j] == texto[i]) 
+                contador += 1;
+        }
+    }
+    return contador;
+}
+function contarConsonantes(letras)
+{
+    let texto = arrayTexto();
+    let contador = 0;
+    let esta = "false";
+    for (let i = 0; i < texto.length; i++) {
+        esta = "true";
+        for (let j = 0; j < letras.length; j++) {
+            if (letras[j] == texto[i] || texto[i] == " " ) 
+                esta = false;
+        }
+        if (esta) 
+            contador += 1;
+    }
+    return contador;
+}
+
+function comprobar()
+{
+    document.formulario.A.value = contar("a"); 
+    document.formulario.E.value = contar("e"); 
+    document.formulario.I.value = contar("i"); 
+    document.formulario.O.value = contar("o"); 
+    document.formulario.U.value = contar("u"); 
+    let arrayVocales = ["a","e","i","o","u"];
+    document.formulario.vocalesTotales.value = contarVocalesTotales(arrayVocales); 
+    document.formulario.consonantesTotales.value = contarConsonantes(arrayVocales); 
 }

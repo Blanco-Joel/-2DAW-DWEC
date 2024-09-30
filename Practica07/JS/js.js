e3f3f9d098c171db8f9d5a9a3668fffdea17e53d
Joel Blanco <joelb5700@gmail.com>
2024-09-30 13:22:22 +0200

    hasta el 7 a medias

--- Practica07/JS/js.js
+++ Practica07/JS/js.js
@@ -15,25 +15,49 @@ function contarLetrasTotales()
 }
 function inicioFinal()
 {
+    let correcto = false;
     let textoIntroducido = document.formulario.nombre.value;
     textoIntroducido = textoIntroducido.toLowerCase().split("");
-    if (textoIntroducido[0] ) {
-        
-    }
-    return textoIntroducido;
+    if (textoIntroducido[0].charCodeAt(0) >= 97 && textoIntroducido[0].charCodeAt(0) <= 165 && textoIntroducido[textoIntroducido.length-1].charCodeAt(0) >= 97 && textoIntroducido[textoIntroducido.length-1].charCodeAt(0) <= 165   ) 
+        correcto = true;
+    return correcto;
+}
+function caracteres()
+{
+    let correcto = false;
+    let textoIntroducido = document.formulario.nombre.value;
+    textoIntroducido = textoIntroducido.toLowerCase().split("");
+    textoIntroducido.forEach(letra => {
+        if (letra.charCodeAt(0) >= 97 && letra.charCodeAt(0) <= 122 && letra.charCodeAt(0) != 170 && letra.charCodeAt(0) != 186 && letra.charCodeAt(0) != 45 && letra.charCodeAt(0) != 32 )
+            correcto = true;
+ 
+    });
+    return correcto;
 }
 function comprobar()
 {
-    let mensaje = ""
-    let mensajeCorrecto = true
-    if (!contarLetrasTotales)
+    console.log(" ".charCodeAt(0));
+    let mensajeTotal = "";
+    let mensajeCorrecto = true;
+    if (!contarLetrasTotales())
     {
-        mensaje += "Longitud inadecuada.\n"  
+        mensajeTotal += "Longitud inadecuada.\n" ;
         mensajeCorrecto = false;  
     }
-    if (!inicioFinal)
+    if (!inicioFinal())
     {
-        mensaje += "El nombre tiene que empezar y terminar con una letra.\n"  
+        mensajeTotal += "El nombre tiene que empezar y terminar con una letra.\n";
         mensajeCorrecto = false;  
+    }
+
+    if (!caracteres())
+    {
+
+        mensajeTotal += "Carácteres inadecuados.\n";
+        mensajeCorrecto = false;  
+    }
+    if (!mensajeCorrecto) 
+    {
+        document.formulario.mensaje.value = mensajeTotal;
     }
 }

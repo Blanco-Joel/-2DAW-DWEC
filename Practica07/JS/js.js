356a544ca73054ec09a43d7dfcf6c277c365b168
Joel Blanco <joelb5700@gmail.com>
2024-09-30 11:54:45 +0200

    7

--- Practica07/JS/js.js
+++ Practica07/JS/js.js
@@ -3,47 +3,37 @@ function inicio()
 {
     document.formulario.comprobar.onclick = comprobar;
 }
-function arrayTexto()
-{
-    let textoIntroducido = document.formulario.cadena.value;
-    textoIntroducido = textoIntroducido.toLowerCase().split("");
-    return textoIntroducido;
-}
-function contar(letra)
-{
-    let texto = arrayTexto();
-    let contador = 0;
-    for (let i = 0; i < texto.length; i++) {
-        if (letra == texto[i]) 
-            contador += 1;
-    }
-    return contador;
-}
+
+
 function contarLetrasTotales()
 {
-    let texto = document.formulario.cadena.value.toLowerCase();
-    let subCadena = document.formulario.subCadena.value.toLowerCase();
-    let contador = 0;
-    let i = 0; 
-    while (i < texto.length) {
-        if (texto.includes(subCadena,i)) 
-        {
-            contador += 1;
-            i = texto.indexOf(subCadena,i) +1;
-        }else
-        {
-            i += 1;
-        }
-    }
-    return contador;
+    let texto = document.formulario.nombre.value.length;
+    let correcto = false;
+    if (texto >= 3 && texto <=27 )
+        correcto = true;
+    return correcto;
 }
-
+function inicioFinal()
+{
+    let textoIntroducido = document.formulario.nombre.value;
+    textoIntroducido = textoIntroducido.toLowerCase().split("");
+    if (textoIntroducido[0] ) {
+        
+    }
+    return textoIntroducido;
+}
 function comprobar()
 {
-    if (document.formulario.subCadena.value.length == 1 ) {
-        document.formulario.mensaje.value = contar(document.formulario.subCadena.value); 
+    let mensaje = ""
+    let mensajeCorrecto = true
+    if (!contarLetrasTotales)
+    {
+        mensaje += "Longitud inadecuada.\n"  
+        mensajeCorrecto = false;  
     }
-    else{
-        document.formulario.mensaje.value = contarLetrasTotales(); 
+    if (!inicioFinal)
+    {
+        mensaje += "El nombre tiene que empezar y terminar con una letra.\n"  
+        mensajeCorrecto = false;  
     }
 }

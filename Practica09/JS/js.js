5b22ab5d93f8640986f58be17022b6e72d4dcd8e
Joel Blanco <joelb5700@gmail.com>
2024-10-01 13:02:44 +0200

    hasta el 7 y 9 a medias

---
+++ Practica09/JS/js.js
@@ -0,0 +1,52 @@
+window.onload = inicio;
+function inicio() 
+{
+    document.formulario.comprobar.onclick = comprobar;
+}
+
+function letraInicio()
+{
+    let correcto = false;
+    let textoIntroducido = document.formulario.email.value.toLowerCase();
+    if (textoIntroducido[0].charCodeAt(0) >= 97 && textoIntroducido[0].charCodeAt(0) <= 122 ) 
+        correcto = true;
+    return correcto;
+}
+function caracteres()
+{
+    let correcto = true;
+    let textoIntroducido = document.formulario.email.value.toLowerCase();
+    let limite = textoIntroducido.search("@")-2;
+    textoIntroducido = textoIntroducido.split("");
+    for (let i = 1; i < limite; i++)
+    {
+            if(!(textoIntroducido[i].charCodeAt(0) >= 97 && textoIntroducido[i].charCodeAt(0) <= 122) || !(textoIntroducido[i].charCodeAt(0) >= 48 && textoIntroducido[i].charCodeAt(0) >= 57 ) || !(textoIntroducido[i].charCodeAt(0) != 45) || !(textoIntroducido[i].charCodeAt(0) != 46))
+                correcto = false;
+
+        console.log(textoIntroducido[i].charCodeAt(0));
+        console.log(correcto);
+    }
+    
+    return correcto;
+}
+function comprobar()
+{
+    let mensajeTotal = "";
+    let mensajeCorrecto = true;
+    if (!letraInicio())
+    {
+        mensajeTotal += "El email tiene que empezar con una letra.\n";
+        mensajeCorrecto = false;  
+    }
+
+    if (!caracteres())
+    {
+
+        mensajeTotal += "Carácteres inadecuados antes del @ \n";
+        mensajeCorrecto = false;  
+    }
+    if (!mensajeCorrecto) 
+    {
+        document.formulario.mensaje.value = mensajeTotal;
+    }
+}

8a1997849f2883cdd4a8dfbf86cf369b1f6e67a0
Joel Blanco <joelb5700@gmail.com>
2024-10-01 13:55:40 +0200

    hasta el 7 y 9 a medias

--- Practica09/JS/js.js
+++ Practica09/JS/js.js
@@ -19,18 +19,26 @@ function caracteres()
     let textoIntroducido = document.formulario.email.value.toLowerCase();
     let limite = textoIntroducido.search("@")-1;
     textoIntroducido = textoIntroducido.split("");
-    console.log(textoIntroducido);
     for (let i = 1; i <= limite; i++)
     {    
             if(!(textoIntroducido[i].charCodeAt(0) >= 97 && textoIntroducido[i].charCodeAt(0) <= 122) && 
-               !(textoIntroducido[i].charCodeAt(0) >= 45 && textoIntroducido[i].charCodeAt(0) <= 57 ))
+               !(textoIntroducido[i].charCodeAt(0) >= 45 && textoIntroducido[i].charCodeAt(0) <= 57 ) ||
+                !(textoIntroducido[i].charCodeAt(0) != 47))
                {
                 correcto = false;
                }
-        console.log(textoIntroducido[i].charCodeAt(0));
-        console.log(!(textoIntroducido[i].charCodeAt(0) >= 97 && textoIntroducido[i].charCodeAt(0) <= 122) && !(textoIntroducido[i].charCodeAt(0) >= 45 && textoIntroducido[i].charCodeAt(0) <= 57 ));
     }
     
+    return correcto;
+}
+function letraFinal()
+{
+    let correcto = true;
+    let textoIntroducido = document.formulario.email.value.toLowerCase();
+    let index =     textoIntroducido.search("@")-1;
+    if (!(textoIntroducido[index].charCodeAt(0) >= 97 && textoIntroducido[index].charCodeAt(0) <= 122) && 
+        !(textoIntroducido[index].charCodeAt(0) >= 48 && textoIntroducido[index].charCodeAt(0) <= 57 ))
+        correcto = false;
     return correcto;
 }
 function comprobar()
@@ -46,6 +54,11 @@ function comprobar()
     if (!caracteres())
     {
         mensajeTotal += "Carácteres inadecuados antes del @ \n";
+        mensajeCorrecto = false;  
+    }
+    if (!letraFinal())
+    {
+        mensajeTotal += "Carácter inadecuado justo antes del @ \n";
         mensajeCorrecto = false;  
     }
     if (!mensajeCorrecto) 

1cea4d4b101c4d2ee6b84b17563482d5c11f1e1f
Joel Blanco <joelb5700@gmail.com>
2024-10-04 08:35:59 +0200

    HECHO

--- Practica10/JS/js.js
+++ Practica10/JS/js.js
@@ -1,13 +1,17 @@
 window.onload = inicio;
-function inicio() 
+
+function inicio()
 {
-    document.formulario.comprobar.onclick = comprobar;
+    document.formulario.boton.onclick = comprobar;
 }
 
-function letraInicio()
+function comprobar()
 {
-
-
+    let cadena = document.formulario.url.value;
+    if(cadena.substring(0,7) == "http://" || cadena.substring(0,8) == "https://" || cadena.substring(0,4) == "www.")
+        comrpobarUrl(cadena);
+    else
+        mensaje();
 }
 
 function comrpobarUrl(cadena)
@@ -23,25 +27,89 @@ function comrpobarUrl(cadena)
     else 
         limite = 4;
 
-}
-function caracteres()
-{
-    let correcto = true;
-    let textoIntroducido = document.formulario.email.value.toLowerCase();
-    let limite = textoIntroducido.search("@")-2;
-    textoIntroducido = textoIntroducido.split("");
-    for (let i = 1; i < limite; i++)
+    if(!comp_letra(cadena.charAt(limite+1)) && !comp_digito(cadena.charAt(limite+1)))
     {
-            if(!(textoIntroducido[i].charCodeAt(0) >= 97 && textoIntroducido[i].charCodeAt(0) <= 122) || !(textoIntroducido[i].charCodeAt(0) >= 48 && textoIntroducido[i].charCodeAt(0) >= 57 ) || !(textoIntroducido[i].charCodeAt(0) != 45) || !(textoIntroducido[i].charCodeAt(0) != 46))
-                correcto = false;
-
-        console.log(textoIntroducido[i].charCodeAt(0));
-        console.log(correcto);
+        console.log("1");
+        mensaje();
+        seguir = false;
     }
-    
-    return correcto;
+
+
+    for (let i = limite+2; i < limite2-2 ; i++)
+    {
+        if(!comp_letra(cadena.charAt(i)) && !comp_digito(cadena.charAt(i)) && !comp_especiales(cadena.charAt(i)))
+        {
+            console.log("2");
+            mensaje();
+            seguir = false;
+        }
+    }
+
+    if(!comp_letra(cadena.charAt(limite2-1)) && !comp_digito(cadena.charAt(limite2-1)))
+    {
+        console.log("3");
+        mensaje();
+        seguir = false;
+    }
+
+    if(finalCadena.length < 2 || finalCadena > 4)
+    {
+        console.log("4");
+        mensaje();
+        seguir = false;
+    }
+
+    for (let i = limite2+1; i < cadena.length ; i++)
+    {
+        
+        if(!comp_letra(cadena.charAt(i)))
+        {
+            console.log("5");
+            mensaje();
+            seguir = false;
+        }
+    }
+
+    if(seguir)
+        document.formulario.mensaje.value = "URL Correcta";
 }
-function comprobar()
+
+function comp_letra(cadena){
+    let seguir=false;
+    for (let i = 65; i <= 90; i++)
+    {
+        if(cadena == String.fromCharCode(i))
+            seguir = true;
+    }
+    for (let i = 97; i <= 122; i++)
+    {
+        if(cadena == String.fromCharCode(i))
+            seguir = true;
+    }
+    return seguir;
+}
+
+function comp_digito(cadena){
+    let seguir=false;
+    for (let i = 0; i <= 9; i++)
+    {
+        if(cadena == (i).toString)
+            seguir = true;
+    }
+    return seguir;
+}
+
+function comp_especiales(cadena){
+    let seguir=false;
+    if(cadena == String.fromCharCode(45))
+        seguir = true;
+    return seguir;
+}
+
+function mensaje()
+{
+    document.formulario.mensaje.value = "URL Incorrecta";
+}function comprobar()
 {
     let mensajeTotal = "";
     let mensajeCorrecto = true;
